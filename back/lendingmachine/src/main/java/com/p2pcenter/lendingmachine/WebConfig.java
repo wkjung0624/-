00c68aef4d5709ea29f69f925d42465ca42c251f
Wangyu Jung <skyship36@gmail.com>
2023-12-28 16:56:38 +0900

    add: 세션 타임아웃 설정

--- back/lendingmachine/src/main/java/com/p2pcenter/lendingmachine/WebConfig.java
+++ back/lendingmachine/src/main/java/com/p2pcenter/lendingmachine/WebConfig.java
@@ -1,12 +1,19 @@
 package com.p2pcenter.lendingmachine;
 
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
+    @Autowired
+    private HttpSession session;
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
@@ -15,5 +22,11 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedHeaders("*")
                 .allowCredentials(true); // 인증 정보 허용
 
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        int secondsInOneYear = 60 * 60; // 1시간 초
+        session.setMaxInactiveInterval(secondsInOneYear);
     }
 }
